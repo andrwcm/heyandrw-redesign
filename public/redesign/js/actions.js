ba53ae4d4f3aa74dfdea1e5af052aaf01fe7a81b
heyAndrw <monoku@MacBook-Pro-de-Monoku.local>
2016-05-04 02:28:38 -0500

    New things
    actualization

--- public/redesign/js/actions.js
+++ public/redesign/js/actions.js
@@ -6,10 +6,52 @@ $(function() {
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        },1600);
+        },1800);
         return false;
       }
     }
   });
 });
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 700) {
+        $(".heyandrw-home").fadeIn("fast");
+    } else {
+        $(".heyandrw-home").fadeOut("fast");
+    }
+
+    if (scroll >= 700) {
+        $(".about").addClass("active");
+    } else {
+        $(".about").removeClass("active");
+    }
+
+    if (scroll >= 1400) {
+        $(".about").removeClass("active");
+    }
+
+    if (scroll >= 1600) {
+        $(".work").addClass("active");
+    } else {
+        $(".work").removeClass("active");
+    }
+
+});
+
+
+
+
+$(document).ready(function(){
+    $(".contact").click(function(){
+    $(".heycontact").fadeIn();
+    });
+
+    $(".close").click(function(){
+    $(".heycontact").fadeOut();
+    });
+
+});
+
 
